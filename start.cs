bb7afcccba37226f317d0628a275db0e9581b04c
helen112345 <39406743+helen112345@users.noreply.github.com>
2018-05-24 15:04:40 +0900

    Add files via upload

---
+++ start.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour
+{
+
+    void Awake()
+    {
+    }
+
+    public void Startbutton()
+    {
+        Invoke("startgame", 5f);
+    }
+
+    void startgame()
+    {
+        Application.LoadLevel("1-Loading");
+    }
+}
+
